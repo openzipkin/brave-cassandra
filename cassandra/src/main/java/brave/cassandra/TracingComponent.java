52ff5ae67f4e116f2007233702a2a297f53fa8bf
Adrian Cole <adrian.f.cole@gmail.com>
2018-09-07 19:10:48 -0400

    Moves to b3 single format

--- cassandra/src/main/java/brave/cassandra/TracingComponent.java
+++ cassandra/src/main/java/brave/cassandra/TracingComponent.java
@@ -14,63 +14,29 @@
 package brave.cassandra;
 
 import brave.Tracer;
-import brave.propagation.Propagation.Getter;
-import brave.propagation.TraceContext;
-import java.nio.ByteBuffer;
-import java.nio.charset.Charset;
-import java.util.Map;
 
 abstract class TracingComponent {
-  static final Charset UTF_8 = Charset.forName("UTF-8");
-
-  /** Getter that pulls trace fields from ascii values */
-  static final Getter<Map<String, ByteBuffer>, String> GETTER =
-      new Getter<Map<String, ByteBuffer>, String>() {
-        @Override public String get(Map<String, ByteBuffer> carrier, String key) {
-          ByteBuffer buf = carrier.get(key);
-          return buf != null ? UTF_8.decode(buf).toString() : null;
-        }
-
-        @Override public String toString() {
-          return "Map::get";
-        }
-      };
 
   abstract Tracer tracer();
-
-  abstract TraceContext.Extractor<Map<String, ByteBuffer>> extractor();
 
   static final class Current extends TracingComponent {
     @Override
     Tracer tracer() {
       return brave.Tracing.currentTracer();
     }
-
-    @Override
-    TraceContext.Extractor<Map<String, ByteBuffer>> extractor() {
-      brave.Tracing tracing = brave.Tracing.current();
-      return tracing != null ? tracing.propagation().extractor(GETTER) : null;
-    }
   }
 
   static final class Explicit extends TracingComponent {
     final Tracer tracer;
-    final TraceContext.Extractor<Map<String, ByteBuffer>> extractor;
 
     Explicit(brave.Tracing tracing) {
       if (tracing == null) throw new NullPointerException("tracing == null");
       this.tracer = tracing.tracer();
-      this.extractor = tracing.propagation().extractor(GETTER);
     }
 
     @Override
     Tracer tracer() {
       return tracer;
-    }
-
-    @Override
-    TraceContext.Extractor<Map<String, ByteBuffer>> extractor() {
-      return extractor;
     }
   }
 }
